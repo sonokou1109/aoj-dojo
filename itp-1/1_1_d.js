e51207d904ac8087f491af1a8209107f316a4796
Koki Sonoda <koki.sonoda1304@gmail.com>
2018-04-30 09:31:41 +0900

    remove Math.floor at second

--- itp-1/1_1_d.js
+++ itp-1/1_1_d.js
@@ -3,6 +3,6 @@ process.stdin.on('data', input => {
   const s = parseInt(input);
   var hour = Math.floor(s / 3600);
   var minute = Math.floor( (s % 3600) / 60 );
-  var second = Math.floor(s % 60);
+  var second = s % 60;
   console.log(hour + ":" + minute + ":" + second);
 });
